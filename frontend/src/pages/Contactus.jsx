b43dd3908488e458201d1a8bfe7e09857ff2fb09
samindika97 <sdalanindu@gmail.com>
2023-10-16 10:20:58 +0530

    Stuling added to contact us form

--- frontend/src/pages/Contactus.jsx
+++ frontend/src/pages/Contactus.jsx
@@ -27,18 +27,18 @@ const ContactUs = () => {
 
     return (
         <div>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <div>
                     <label htmlFor="name">Name</label>
                     <input
                         type="text"
                         id="name"
-                        className={errors.name && touched.name ? "input-error" : ""}
+                        className={errors.name && touched.name ? "shadow appearance-none border border-redDefault-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" : "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"}
                         value={values.name}
                         onBlur={handleBlur}
                         onChange={handleChange}
                     />
-                    {errors.name && touched.name && <p className="error">{errors.name}</p>}
+                    {errors.name && touched.name && <p className="text-redDefault-500 text-xs italic">{errors.name}</p>}
 
                     
                 </div>
@@ -47,26 +47,26 @@ const ContactUs = () => {
                     <input
                         type="email"
                         id="email"
-                        className={errors.email && touched.email ? "input-error" : ""}
+                        className={errors.email && touched.email ? "shadow appearance-none border border-redDefault-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" : "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"}
                         value={values.email}
                         onBlur={handleBlur}
                         onChange={handleChange}
                     />
-                    {errors.email && touched.email && <p className="error">{errors.email}</p>}
+                    {errors.email && touched.email && <p className="text-redDefault-500 text-xs italic">{errors.email}</p>}
                 </div>
                 <div>
                     <label htmlFor="message">Message</label>
                     <textarea
                         id="message"
-                        className={errors.message && touched.message ? "input-error" : ""}
+                        className={errors.message && touched.message ? "shadow appearance-none border border-redDefault-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" : "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"}
                         value={values.message}
                         onBlur={handleBlur} 
                         onChange={handleChange}
                     ></textarea>
-                    {errors.message && touched.message && <p className="error">{errors.message}</p>}
+                    {errors.message && touched.message && <p className="text-redDefault-500 text-xs italic">{errors.message}</p>}
                 </div>
 
-                <input type="submit" className="bg-primary" value="Submit" />
+                <input type="submit" className="bg-blueDefault-500 hover:bg-blueDefault-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" value="Submit" />
             </form>
         </div>
     );
