3d7e1e6885e5d54918a60508a4777e8d8c584bbb
Hollin-A <ariyawansahollin@gmail.com>
2024-01-27 00:28:39 +0530

    ui changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,16 +7,20 @@ import { featuresTabs } from "../data/data";
 
 const Home = () => {
   return (
-    <div className="h-full">
-      <div className="my-10 flex h-96 justify-between">
+    <div className="flex h-full flex-col justify-between gap-10">
+      <div className="flex h-96 justify-between">
         <div className="flex w-1/2 flex-col items-start justify-center">
           <p className="text-5xl font-bold text-blue">
             Unlocking Wellness:
             <br />
             Your Gateway to <span className="text-teal">Better Health</span>
           </p>
-          <p className="mt-3 w-4/5 text-sm text-blue text-justify">
-            <br/>Welcome to MedPlus, where your well-being takes center stage. Discover accurate health information, find nearby healthcare facilities, and take control of your health journey. It's time to prioritize your health with MedPlus.
+          <p className="mt-3 w-4/5 text-justify text-sm text-blue">
+            <br />
+            Welcome to MedPlus, where your well-being takes center stage.
+            Discover accurate health information, find nearby healthcare
+            facilities, and take control of your health journey. It's time to
+            prioritize your health with MedPlus.
           </p>
           <button className="mt-3 rounded-3xl bg-teal px-10 py-1">
             <p className="font-semibold uppercase text-white">sign in</p>
